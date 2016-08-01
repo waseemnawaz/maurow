abace778284108f776b681a6176040947a628845
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-08-01 13:32:36 +0200

    added static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def inicio
+  end
+
+  def ingles
+  end
+
+  def ciencia
+  end
+
+  def catering
+  end
+
+  def contacto
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
+
+  get 'static_pages/inicio'
+
+  get 'static_pages/ingles'
+
+  get 'static_pages/ciencia'
+
+  get 'static_pages/catering'
+
+  get 'static_pages/contacto'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

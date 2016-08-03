c8732a67900279ff88492dbd1ade50e9c8247129
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-08-03 14:31:45 +0200

    added contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,4 +16,14 @@ class StaticPagesController < ApplicationController
 
   def contacto
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'waseemhijodenawaz@yahoo.es',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/contacto'
 
+  post 'static_pages/thank_you'
+
+  #get 'static_pages/thank_you'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

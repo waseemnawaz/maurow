6819d0ae75a18a4eb654e2bec4779327aaa3b7bb
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-08-09 12:32:23 +0200

    Updated AWS S3 Config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,5 +7,5 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
     :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]                     # required
+  config.fog_directory  = ENV["AWS_BUCKET_NAME"]                     # required
 end

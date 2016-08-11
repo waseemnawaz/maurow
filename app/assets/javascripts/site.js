2d2117a8bcf9f1e013b66cb4de55cc6ee50c9419
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-08-11 14:21:50 +0200

    page styling

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,29 @@
 $(document).on('ready page:load', function(){
-	$(function(){
-        $('.textz').succinct({
-            size: 120
-        });
+	//succinct
+	//$(function(){
+    //$('.textz').succinct({
+      //size: 120
+    //});
+  //});
+  //smooth scrolling
+  var $root = $('html, body');
+  $('.navbar-nav a').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+       scrollTop: $(href).offset().top
+    }, 500, function () {
+       window.location.hash = href;
     });
+    return false;
+   });
+	//collapser
+	$('.textz').collapser({
+    mode: 'words',
+    truncate: 20
+});
+	//elevate zoom
+	$(".img-zoom").elevateZoom({
+  	zoomType				: "inner",
+  	cursor: "crosshair"
+	});
 });

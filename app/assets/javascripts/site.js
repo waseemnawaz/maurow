4de395f3d6c81c9da94883da0f017b2385b69cf1
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-08-16 14:07:47 +0200

    eliminate elevate zoom

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -6,24 +6,28 @@ $(document).on('ready page:load', function(){
     //});
   //});
   //smooth scrolling
-  var $root = $('html, body');
-  $('.navbar-nav a').click(function() {
-    var href = $.attr(this, 'href');
-    $root.animate({
-       scrollTop: $(href).offset().top
-    }, 500, function () {
-       window.location.hash = href;
-    });
-    return false;
-   });
+  //var $root = $('html, body');
+  //$('.navbar-nav a').click(function() {
+    //var href = $.attr(this, 'href');
+    //$root.animate({
+       //scrollTop: $(href).offset().top
+    //}, 500, function () {
+       //window.location.hash = href;
+    //});
+    //return false;
+   //});
 	//collapser
 	$('.textz').collapser({
     mode: 'words',
     truncate: 20
-});
+  });
 	//elevate zoom
 	//$(".img-zoom").elevateZoom({
   	//zoomType				: "inner",
   	//cursor: "crosshair"
 	//});
+  //carousel
+  $(function(){
+  $('#thecarousel').carousel();
+});
 });

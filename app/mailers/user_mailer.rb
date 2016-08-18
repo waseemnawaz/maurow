a48f43cdb80fb3a6cc97a6baa3c59f94f931f7ed
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-08-18 11:49:38 +0200

    changed usermailer again

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def contact_form(email, name, message)
   @message = message
-    mail(:from => email,
+    mail(:from => "contact@maurowgomez.com",
         :to => 'maurowgomez@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
